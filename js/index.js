4be5348d054a45140ec3aa94d31fdc908b017b78
Roman <saldanroman@gmail.com>
2019-03-08 23:41:59 +0000

    Create Ajax get html page

---
+++ js/index.js
@@ -0,0 +1,15 @@
+document.querySelector('.page-loaded').innerTrext = (new Date()).toLocaleTimeString();
+
+document.querySelector('ajax-get-html').addEventListner('click', ajaxGetHtml);
+
+function ajaxGetHtml(){
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState===4 && xhr.status===200){
+            document.querySelector('.html-container').innerHTML=xhr.responseText;
+        }
+    }
+    xhr.open('GET', 'base-html-document.html', true);
+    xhr.send();
+    
+}
