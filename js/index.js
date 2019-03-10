8ab02da1becd4d06c4cd56d303635d5441ca3611
SaldanRoman <34034422+SaldanRoman@users.noreply.github.com>
2019-03-10 02:00:47 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -18,7 +18,11 @@ document.querySelector('.login-form').addEventListener('submit', submitForm);
 
 function submitForm(e) {
     e.preventDefault();
-    fetch('form.php', {method: 'POST',body: new FormData(document.querySelector('.login-form'))})
+    fetch('form.php', {
+        method: 'POST',
+        body: new FormData(document.querySelector('.login-form'))
+    })
         .then( response => response.text() )
-        .then( html => document.querySelector('.server-response').innerHTML = html );
+        .then( html => document.querySelector('.server-response')
+                                .innerHTML = html );
 }
